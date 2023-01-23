35fc9b4fba8b511026dffc6632147fb55d5e7a06
tyange <usun16@gmail.com>
2023-01-23 11:47:08 +0900

    url parameter 값에 따라 데이터 다르게 가져오기

--- src/components/burger-item.tsx
+++ src/components/burger-item.tsx
@@ -1,11 +1,14 @@
 import { JSX } from "solid-js";
-import { BurgerData } from "~/routes";
 
-export default function Burger({
+export default function BurgerItem({
   Name: name,
   Brand: brand,
   Description: description,
-}: BurgerData): JSX.Element {
+}: {
+  Name: string;
+  Brand: string;
+  Description: string;
+}): JSX.Element {
   return (
     <div class="border shadow-sm w-full h-full rounded-xl flex flex-col p-5">
       <span>{name}</span>
